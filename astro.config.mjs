14e9f9f394c665c247f8292bca1ce94371b0bbef
Richard-Dent <142474859+Richard-Dent@users.noreply.github.com>
2024-07-08 21:36:07 -0400

    adjusted astro config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://richard-dent.github.io',
-    base: '/first-astro-website',
+    base: '/first-astro-website/',
 });
